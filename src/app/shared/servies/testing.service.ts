7ce940a79181e21e1e1b21edfc41e8c135b7749c
Sameerijaz112 <sameerejaz970@gmail.com>
2022-10-05 09:45:00 -0700

    info

--- src/app/shared/servies/testing.service.ts
+++ src/app/shared/servies/testing.service.ts
@@ -8,7 +8,7 @@ export class TestingService {
 
   constructor(private Httpclient:HttpClient) { } //HttpClient k use sy hum Post Get or dosry Http k option use kr skty hein//or network k oper data bhejta hai HttpClient
   postmydatatobackend (payload:any) {
-  return this.Httpclient.post('',payload)
+  return this.Httpclient.post('http://localhost:5555/formValue/formApi',payload)
   }
   getmydatatobackend(){
     return
