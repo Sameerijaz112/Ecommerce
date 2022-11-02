2a070aa24a79a3a8998740d5fafb724bfea1fb47
Sameerijaz112 <sameerejaz970@gmail.com>
2022-11-02 02:28:59 -0700

    new

--- src/app/admin-module/adminComponent/product/product.component.ts
+++ src/app/admin-module/adminComponent/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-product',
@@ -7,10 +8,15 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  color:any=["Red","Black","Blue"]
   selectSize:any=['small', 'medium' ,'large','xlarge'];
   categoryArray:any=['Men','Female']
+  newSizeArray: any = [];
+  imageArray: any = [];
+  disableButtonTrue: boolean = false;
+  FileSelect: any;
   ProductForm:FormGroup|any 
+  
 
 
   constructor(private formbuilder:FormBuilder) { 
@@ -21,7 +27,7 @@ export class ProductComponent implements OnInit {
   }
 FormProductModel(){
   this.ProductForm=this.formbuilder.group({
-    productName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]],
+    productName: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]],
     productQuantity:['',[Validators.required,Validators.pattern('^[0-9]$')]],
     productPrice:['',[Validators.required,Validators.pattern('^[0-9]$')]],
     productMaterial:['',[Validators.required,Validators.pattern(/^[A-Za-z ]+$/)]],
@@ -32,11 +38,33 @@ FormProductModel(){
     category:['',Validators.required]
   })
 }
+getSize(event: any ){
+if (event.target.checked) {
+  this.newSizeArray.push(event.target.value)
+} else {
+  this.newSizeArray = this,this.newSizeArray.filter((value:any) => ! event.target.value)
+} 
+}
 
+getimages(event: any){
+
+if (event.target.files.length <= 5) {
+  [... event.target.files].forEach(file => this.imageArray.push(file));
+  this.imageArray;
+} else {
+  this.imageArray = [];
+  this.FileSelect.nativeElement.value= null;
+  this.disableButtonTrue = true;
+}
+
+}
 
 formSubmit(){
-  let formValue=this.ProductForm.value 
-  console.log(formValue)
+ this.newSizeArray.forEach((element: string) => {
+  let formControl = new FormControl(element)
+  this.ProductForm.get("size").push(formControl)
+
+ });
 }
 }
 
