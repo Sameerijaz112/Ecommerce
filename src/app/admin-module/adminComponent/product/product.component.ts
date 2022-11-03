c6b8f62ff8407733f82f03a0fc1e2fab21539104
Sameerijaz112 <sameerejaz970@gmail.com>
2022-11-03 02:25:03 -0700

    some error

--- src/app/admin-module/adminComponent/product/product.component.ts
+++ src/app/admin-module/adminComponent/product/product.component.ts
@@ -9,6 +9,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 })
 export class ProductComponent implements OnInit {
   @ViewChild('FilesSelect') FileSelect: ElementRef | any
+
   color:any=["Red","Black","Blue"]
   selectSize:any=['small', 'medium' ,'large','xlarge'];
   categoryArray:any=['Men','Female']
@@ -28,15 +29,15 @@ export class ProductComponent implements OnInit {
   }
 FormProductModel(){
   this.ProductForm=this.formbuilder.group({
-    productName: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(100)]],
-    productQuantity:['',[Validators.required,Validators.pattern('^[0-9]$')]],
-    productPrice:['',[Validators.required,Validators.pattern('^[0-9]$')]],
-    productMaterial:['',[Validators.required,Validators.pattern(/^[A-Za-z ]+$/)]],
-    description:['',Validators.required],
-    logoMaterial:['',[Validators.required,Validators.pattern(/^[A-Za-z ]+$/)]],
+    productName:  new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+    productQuantity:new FormControl('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+    productPrice: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+    productMaterial: new FormControl('', Validators.required),
+    description: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
+    logoMaterial: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
     size:new FormArray([]),
-    color: ['',[Validators.required,Validators.pattern(/^[A-Za-z ]+$/)]],
-    category:['',Validators.required]
+    color: new FormControl('', [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]),
+    category:new FormControl('', Validators.required),
   })
 }
 getSize(event: any ){
@@ -68,4 +69,8 @@ formSubmit(){
  });
 }
 }
+/* MultipartformDAta */
+let MultiPartFormData = new FormData();
+MultiPartFormData.append('productName', this.);
+
 
